f7c4be2c6fd577b91e0123836900a53058f1f272
miaochenxi <miaochenxi@outlook.com>
2022-09-29 00:31:07 +0800

    鉴权中间件加入等级参数；添加博客路由；

--- src/api/api.routes.ts
+++ src/api/api.routes.ts
@@ -1,6 +1,7 @@
 import Router from '@koa/router';
+import blogRouter from './blog';
 import userRouter from './user';
 
 const apiRouter = new Router({ prefix: '/api' });
 
-export default apiRouter.use(userRouter.routes());
+export default apiRouter.use(userRouter.routes(), blogRouter.routes());
